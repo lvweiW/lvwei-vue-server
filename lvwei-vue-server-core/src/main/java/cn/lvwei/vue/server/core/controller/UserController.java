63d6a2a9c7ed50fbcb20d84082415f03ad0e4181
lvwei <lvwei1000@gmail.com>
2024-06-07 16:16:55 +0800

    完成用户登录接口

--- lvwei-vue-server-core/src/main/java/cn/lvwei/vue/server/core/controller/UserController.java
+++ lvwei-vue-server-core/src/main/java/cn/lvwei/vue/server/core/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-@Tag(name = "user", description = "用户相关接口")
+@Tag(name = "用户相关接口", description = "用户相关接口")
 public class UserController {
 
     @Autowired
--- lvwei-vue-server-deploy/src/main/java/cn/lvwei/vue/server/deploy/ServerApplication.java
+++ lvwei-vue-server-deploy/src/main/java/cn/lvwei/vue/server/deploy/ServerApplication.java
@@ -3,15 +3,20 @@ package cn.lvwei.vue.server.deploy;
 
 import cn.hutool.core.util.ObjectUtil;
 import cn.lvwei.vue.server.common.utils.LogUtil;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+
 @SpringBootApplication
+@ComponentScan(basePackages = "cn.lvwei.vue.server")
+@MapperScan("cn.lvwei.vue.server.core.mapper")
 public class ServerApplication {
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext application = SpringApplication.run(ServerApplication.class, args);
@@ -19,11 +24,12 @@ public class ServerApplication {
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = ObjectUtil.defaultIfBlank(env.getProperty("server.servlet.context-path"), "");
-        LogUtil.info("\n----------------------------------------------------------\n\t" +
-                "Application lvwei-vue-server is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
-                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
-                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
-                "----------------------------------------------------------");
+        LogUtil.info(STR."""
+----------------------------------------------------------
+\tApplication lvwei-vue-server is running! Access URLs:
+\tLocal: \t\thttp://localhost:\{port}\{path}/
+\tExternal: \thttp://\{ip}:\{port}\{path}/
+\tSwagger文档: \thttp://\{ip}:\{port}\{path}/doc.html
+----------------------------------------------------------""");
     }
 }
